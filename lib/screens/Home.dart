d64e501e85798f8b8c581746d90372d3ef50c287
Valery Ruth Ayasta Leonardo <102206366+ValeryAyasta@users.noreply.github.com>
2023-11-26 21:29:44 -0500

    agregar el icono y branding

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -15,8 +15,11 @@ class _HomeState extends State<Home> {
   int _selectedTab=0;
 
   final List<Widget> _children=[
+    Center(
+      child: Image.network("https://psicologiamonzo.com/wp-content/uploads/2019/03/multiracial-grupo-joven-gente-toma-selfie_1139-1032.jpg"),
+    ),
     const UserList(),
-    const UserFavorites()
+    const UserFavorites(),
   ];
 
   _changeTab(int index){
@@ -38,8 +41,10 @@ class _HomeState extends State<Home> {
         selectedItemColor: Colors.red,
         unselectedItemColor: Colors.grey,
         items: const[
+          BottomNavigationBarItem(icon: Icon(Icons.star), label: "Branding"),
           BottomNavigationBarItem(icon: Icon(Icons.home), label: "Personas"),
           BottomNavigationBarItem(icon: Icon(Icons.favorite), label: "Favorites"),
+
         ],
       ),
     );
